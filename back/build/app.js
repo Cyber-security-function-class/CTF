003b2123a0cad3636b0f9479365f410cb1ec0af4
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-04-11 23:32:14 +0900

    index.ts added

--- back/build/app.js
+++ back/build/app.js
@@ -1,17 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+// import { Routes } from "./routes/routes"; TODO router
+var bodyParser = require("body-parser");
+var errorhandler = require("strong-error-handler");
 var App = /** @class */ (function () {
+    // public routes: Routes = new Routes();
     function App() {
-        this.application = express();
+        this.app = express();
+        this.config();
+        // this.routes.routes(this.app);
+        this.errorHandler();
     }
+    App.prototype.config = function () {
+        this.app.use(bodyParser.json({ limit: '5mb' }));
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+    };
+    App.prototype.errorHandler = function () {
+        this.app.use(errorhandler({
+            debug: process.env.ENV !== 'prod',
+            log: true,
+        }));
+    };
     return App;
 }());
-var app = new App().application;
-app.get("/", function (req, res) {
-    res.send("helloworld");
-});
-app.listen(3000, function () {
-    console.log("start");
-});
+exports.default = new App().app;
 //# sourceMappingURL=app.js.map
---
+++ back/src/index.ts
@@ -0,0 +1,10 @@
+import {createServer} from 'http'
+import app from './app'
+
+const port = process.env.PORT || 7000;
+
+(async () => {
+    createServer(app).listen(port,() => {
+        console.info(`Server running on port ${port}`)
+    });
+})();
