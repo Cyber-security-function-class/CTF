23e1213d3760c634c52133f1d04f56f0c8d0bf53
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-04-14 11:54:07 +0900

    delete signUpTest

--- back/src/app/controllers/auth/index.ts
+++ back/src/app/controllers/auth/index.ts
@@ -9,15 +9,4 @@ const routes = express.Router()
 
 // routes.post("/signUp",)
 routes.post("/signUp",signUpValidator() ,authController.signUp)
-routes.post("/signUpTest",signUpValidator(), (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-    else {
-        console.log(req.body)
-        return res.json({result:true})
-    }
-});
 export default routes
